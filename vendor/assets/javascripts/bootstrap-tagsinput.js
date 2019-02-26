ff8f0456c605a0c82a9323afe93f0398091eb123
pbelasco <pbelasco@gmail.com>
2019-02-26 13:43:57 -0300

    Badges instead of labels

--- vendor/assets/javascripts/bootstrap-tagsinput.js
+++ vendor/assets/javascripts/bootstrap-tagsinput.js
@@ -3,7 +3,7 @@
 
   var defaultOptions = {
     tagClass: function(item) {
-      return 'badge badge-lg badge-info';
+      return 'badge badge-info';
     },
     itemValue: function(item) {
       return item ? item.toString() : item;
